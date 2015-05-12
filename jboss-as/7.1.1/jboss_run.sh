aeff54d30920756b058b177e7cf8c8b25e47d7da
pGrimaud <pascalgrimaud@gmail.com>
2015-05-12 07:34:49 +0200

    update jboss 7.1.1

--- jboss-as/7.1.1/jboss_run.sh
+++ jboss-as/7.1.1/jboss_run.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-echo "Starting container..."
-
-if [ "$JBOSS_PASS" = "**Random**" ]; then
-    unset JBOSS_PASS
-fi
+echo "Starting container : JBoss AS 7.1.1"
 
 # change the password
 if [ ! -f /.jboss_admin_pass_configured ]; then
 	# generate password
 	PASS=${JBOSS_PASS:-$(pwgen -s 12 1)}
-	# add special caracter at the end --> !
-	PASS=$PASS!
 	_word=$( [ ${JBOSS_PASS} ] && echo "preset" || echo "random" )
 
-	echo "=> Configuring admin user with a ${_word} password in JBoss"
+	echo "Configuring admin user with a ${_word} password in JBoss"
 	/jboss-as-7.1.1.Final/bin/add-user.sh --silent=true admin ${PASS}
-	echo "=> Done!"
+	echo "Done!"
 	touch /.jboss_admin_pass_configured
 
 	echo "================================================================================"
 	echo "You can now configure to this JBoss server using:"
 	echo ""
-	echo "    admin:${PASS}"
+	echo "    Username : admin"
+	echo "    Password : ${PASS}"
 	echo ""
 	echo "================================================================================"
 else
