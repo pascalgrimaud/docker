4e52505a083f5c3f7645058635cc20f3dc7852eb
pGrimaud <pascalgrimaud@gmail.com>
2015-05-29 22:36:33 +0200

    add mysql docker

--- mysql/README.md
+++ mysql/README.md
@@ -1,3 +1,5 @@
 <a href="https://www.mysql.com/" target="_blank">
     <img src="https://www.mysql.com/common/logos/logo-mysql-110x57.png"/>
 </a>
+
+working...
---
+++ mysql/mysql_run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "Starting container : mysql 5.6"
+
+
+# change the password
+if [ ! -f /.password ]; then
+	echo "Initializing the user password..."
+
+	# generate password
+	PASS=${MYSQL_PASS:-$(date +%s | sha256sum | base64 | head -c 16 ; echo)}
+	# change password
+    # ---> put code here
+    # touch this file ...
+	touch /.password
+
+	echo "Initializing the user password : done!"
+fi
+
+# display info
+echo ""
+echo "######################################################################"
+echo "You can now connect to this mysql database using :"
+echo ""
+echo "    <information>"
+if [ ! -d ${PASS} ]; then
+	echo "    with the password '$PASS'"
+	echo ""
+	echo "Please remember to change the above password as soon as possible!"
+else
+	echo "    and enter the password"
+fi
+echo ""
+echo "######################################################################"
+echo ""
+
+# start service
+# ---> put start service here
